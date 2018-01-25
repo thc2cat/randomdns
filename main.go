4620e8db077d176694e813fe343074fec7aa2823
thc <thc2cat@gmail.com>
2018-01-25 09:32:10 +0100

    v0.2 not so random

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 //
 // generate random dns queries
 // usefull when you want to test your DNS queries
-// without cache
+// (unbound as transparent DNS resolver)
 
 package main
 
@@ -21,10 +21,10 @@ func main() {
 	for {
 		ipstring := ""
 		for i := 1; i < 4; i++ {
-			ipstring += strconv.Itoa(rand.Intn(255))
+			ipstring += strconv.Itoa(rand.Intn(235) + 10)
 			ipstring += "."
 		}
-		ipstring += strconv.Itoa(rand.Intn(255))
+		ipstring += strconv.Itoa(rand.Intn(9) + 1)
 
 		fmt.Print(ipstring, " : ")
 		hostname, _ := net.LookupAddr(ipstring)
